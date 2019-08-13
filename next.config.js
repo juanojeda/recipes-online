9c01fe7636447826c1400953d132ea7b808efc75
Juan Ojeda <juan.ojeda@rea-group.com>
2019-08-13 21:48:58 +1000

    init commit

---
+++ next.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  webpack: config => {
+    // add modules
+    config.module.rules.push({
+      test: /\.md$/,
+      use: 'raw-loader'
+    });
+
+    // Fixes npm packages that depend on `fs` module
+    config.node = {
+      fs: 'empty'
+    };
+
+    return config;
+  }
+};
---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Link from 'next/link';
+import Head from '../components/head';
+import { Grommet } from 'grommet';
+
+const Home = () => (
+  <Grommet plain>
+    <Head title="Home" />
+  </Grommet>
+);
+
+export default Home;
